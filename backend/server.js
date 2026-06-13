45dbe53c9822071ac92eb3ef238e8074ba88ed69
Ushasree <ushasree.gattu5@gmail.com>
2026-06-13 09:59:10 +0530

    updated demo

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv').config()
 const cors = require('cors')
 const productRoutes = require('./routes/productRoutes')
 
-app.use(cors())
+app.use(cors({origin:"https://demo-box4zonfv-usha2.vercel.app/"}))
 app.use(express.json())
 
 connectDb()
